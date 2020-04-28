05cd7a3fafd666f09a3b53eabc1ad42ca14e66e4
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-04-28 16:54:47 +0600

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,37 @@
+
+package hello;
+
+
+public class LinearSearch {
+    // sometimes we need to find out any array of any specefic value
+    // then how can we find it ??
+    // here is the logic of linear search 
+    // we simply using a method 
+    // that takes to perameter 
+    // one of array and another of our destination 
+    // .....................let's start ...............
+    
+    public static int  linear_search(int arr[], int key)// two local variable 
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+               return i;
+            }
+        }
+        return -1;
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,20,30,40,50,6,70,80};
+        int k = 6;
+        
+        System.out.println("k is in the index of : " + linear_search(arr,k));
+               
+        
+    }
+}
